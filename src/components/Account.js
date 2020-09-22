4097fa656996782bcb7dbb4792065ab48666f80e
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-09-22 14:13:41 -0500

    hooked up Account.js to redux store, still missing ability to disband tickets?

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getWhoIAm } from '../actions';
+function Account(props) {
+  // props has username, fetching, error,
+  // and the big two are userIssues, and userAnswers
+  useEffect(() => {
+  // grabbing those props again, they _should_ exist
+  // but sometimes they *poof* (on refresh)
+    props.getWhoIAm();
+  }, []) 
 
-export default function Account() {
-  
+
   return (
     <p>Account</p>
   )
 }
+
+
+
+
+function mapStateToProps(state) {
+  return {
+    username: state.accountInfo.username,
+    fetching: state.fetching,
+    error: state.error,
+    userIssues: state.userIssues,
+    userAnswers: state.userAnswers,   
+  }
+}
+
+export default connect(mapStateToProps, { getWhoIAm })(Account)
