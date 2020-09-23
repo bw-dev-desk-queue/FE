d716cd6918c5a2e30a7f12a99af65c14f8ede7fa
justin-jones <52481185+justinemanueljones@users.noreply.github.com>
2020-09-23 13:15:53 -0400

    feature/tickets.js

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -1,35 +1,29 @@
 import React, { useEffect} from 'react';
 import { getAllIssues } from '../actions';
 import { connect } from 'react-redux';
-
 function Tickets(props) {
+  useEffect(() => props.getAllIssues(), [])
+  // this will load all issues into 'props.issues'
+  // there is also a boolean 'props.fetching'
+  // that is true if currently requesting issues
+  // and false if not.
+  if (props.fetching === false && props.issues.length > 0) {
+    console.log("Tickets.js:" + props.issues)
+  }
   return (
     <div >
        <p>Tickets</p>
       {props.tickets.map(ticket => {
-        <div key={ticket.id}>
+        return (<div key={ticket.id}>
           <h2>{ticket.name}</h2>
           <p>{ticket.description}</p>
           <p>{ticket.wit}</p>
           <p>{ticket.category}</p>
-        </div>
+        </div>)
       })}
     </div>
   );
 };
-  // this will load all issues into 'props.issues'
-  useEffect(() => props.getAllIssues(), [])
-  // there is also a boolean 'props.fetching'
-  // that is true if currently requesting issues
-  // and false if not.
-  
-  if (props.fetching === false && props.issues.length > 0) {
-    console.log("Tickets.js:" + props.issues)
-  }
-  
- 
-
-
 const mapStateToProps = (state) => {
   return {
     issues: state.issues,
@@ -37,5 +31,4 @@ const mapStateToProps = (state) => {
     fetching: state.fetching,
   }
 }
-
 export default connect(mapStateToProps, { getAllIssues})(Tickets);
